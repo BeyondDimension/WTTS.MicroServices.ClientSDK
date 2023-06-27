a611baaa1e8591205767ba646e0e6e56e369802a
Aigio Liu <lysoft.pw@foxmail.com>
2023-06-27 18:06:20 +0800

    Update AssemblyInfo.Xaml

---
+++ src/AssemblyInfo.Xaml.cs
@@ -0,0 +1,2 @@
+[assembly: Avalonia.Metadata.XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.Models")]
+[assembly: Avalonia.Metadata.XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.Models.Abstractions")]
