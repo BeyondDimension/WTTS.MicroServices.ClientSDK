ea698f1a77890622be347da04aa72f2d3ddf2698
BuknSS <284912933@qq.com>
2023-08-08 17:56:12 +0800

    🐛 修复 MemoryPack 序列化报错

--- src/BD.WTTS.Primitives.Models/Models/Basics/OfficialMessageItemDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Basics/OfficialMessageItemDTO.cs
@@ -1,20 +1,4 @@
-namespace BD.WTTS.Models;
-
-[MPObj, MP2Obj(SerializeLayout.Explicit)]
-public partial class OfficialMessageTypeDTO
-{
-    /// <summary>
-    /// 官方消息类型
-    /// </summary>
-    [MPKey(0), MP2Key(0)]
-    public OfficialMessageType Type { get; set; }
-
-    /// <summary>
-    /// 类型名称
-    /// </summary>
-    [MPKey(1), MP2Key(1)]
-    public string Name { get; set; } = string.Empty;
-}
+﻿namespace BD.WTTS.Models;
 
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
 public partial class OfficialMessageItemDTO
