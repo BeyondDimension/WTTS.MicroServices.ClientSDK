b6edb88baeb321dc8b5569f80b7a1fb9deb0abda
lxy829 <3346435289@qq.com>
2023-09-12 17:37:37 +0800

    🎨 抽奖活动添加枚举

---
+++ src/BD.WTTS.Primitives/Enums/Raffle/ActivityType.cs
@@ -0,0 +1,17 @@
+namespace BD.WTTS.Enums;
+
+/// <summary>
+/// 活动类型
+/// </summary>
+public enum ActivityType
+{
+    /// <summary>
+    /// 即时抽奖
+    /// </summary>
+    OnlineRaffle = 1,
+
+    /// <summary>
+    /// 到期开奖
+    /// </summary>
+    WaitingRaffleResult = 2
+}
---
+++ src/BD.WTTS.Primitives/Enums/Raffle/ConditionType.cs
@@ -0,0 +1,28 @@
+namespace BD.WTTS.Enums;
+
+/// <summary>
+/// 条件类型
+/// </summary>
+public enum ConditionType
+{
+    /// <summary>
+    /// 年龄限制
+    /// </summary>
+    Age = 1,
+
+    /// <summary>
+    /// 用户级别
+    /// </summary>
+    UserLevel = 2,
+
+    /// <summary>
+    /// 地理位置
+    /// </summary>
+    GeographicLocation = 3,
+
+    /// <summary>
+    /// AppId愿望单匹配
+    /// </summary>
+    WishListMatching = 4,
+
+}
