4b6b46d05562673836a3f175b99e1ba023455051
lxy829 <3346435289@qq.com>
2023-09-14 14:38:37 +0800

    🎨 Add RaffleActivityDetailDTO

--- src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDetailDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDetailDTO.cs
@@ -2,7 +2,7 @@
 namespace BD.WTTS.Models;
 
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
-public partial class RaffleActivityDTO
+public partial class RaffleActivityDetailDTO
 {
     [MPKey(0), MP2Key(0)]
     public Guid Id { get; set; }
@@ -43,4 +43,15 @@ public partial class RaffleActivityDTO
     [MPKey(12), MP2Key(12)]
     public DateTimeOffset RaffleRevealTime { get; set; }
 
+    /// <summary>
+    /// 奖品列表
+    /// </summary>
+    [MPKey(13), MP2Key(13)]
+    public RaffleActivityPrizeDTO[]? Prizes { get; set; }
+
+    /// <summary>
+    /// 条件列表
+    /// </summary>
+    [MPKey(14), MP2Key(14)]
+    public RaffleConditionDTO[]? Conditions { get; set; }
 }
