0abf47fb6630b8c2ec839860abcdd960897fa2d8
lxy829 <3346435289@qq.com>
2023-09-15 16:28:18 +0800

    🎨  Raffle模型调整

--- src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDTO.cs
@@ -17,30 +17,18 @@ public partial class RaffleActivityDTO
     public string? CoverUrl { get; set; }
 
     [MPKey(4), MP2Key(4)]
-    public string? BackgroundImg { get; set; }
-
-    [MPKey(5), MP2Key(5)]
     public DateTimeOffset StartTime { get; set; }
 
-    [MPKey(6), MP2Key(6)]
+    [MPKey(5), MP2Key(5)]
     public DateTimeOffset EndTime { get; set; }
 
-    [MPKey(7), MP2Key(7)]
-    public bool Recalibration { get; set; }
-
-    [MPKey(8), MP2Key(8)]
-    public string ActivityRules { get; set; } = string.Empty;
-
-    [MPKey(9), MP2Key(9)]
-    public string RewardRules { get; set; } = string.Empty;
-
-    [MPKey(10), MP2Key(10)]
+    [MPKey(6), MP2Key(6)]
     public ActivityType ActivityType { get; set; }
 
-    [MPKey(11), MP2Key(11)]
+    [MPKey(7), MP2Key(7)]
     public DateTimeOffset RaffleOpeningTime { get; set; }
 
-    [MPKey(12), MP2Key(12)]
+    [MPKey(8), MP2Key(8)]
     public DateTimeOffset RaffleRevealTime { get; set; }
 
 }
--- src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDetailDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDetailDTO.cs
@@ -11,63 +11,60 @@ public partial class RaffleActivityDetailDTO
     public string ActivityName { get; set; } = string.Empty;
 
     [MPKey(2), MP2Key(2)]
-    public string? Describe { get; set; }
+    public string? GameStoreLink { get; set; }
 
     [MPKey(3), MP2Key(3)]
-    public string? CoverUrl { get; set; }
-
-    [MPKey(4), MP2Key(4)]
     public string? BackgroundImg { get; set; }
 
-    [MPKey(5), MP2Key(5)]
+    [MPKey(4), MP2Key(4)]
     public DateTimeOffset StartTime { get; set; }
 
-    [MPKey(6), MP2Key(6)]
+    [MPKey(5), MP2Key(5)]
     public DateTimeOffset EndTime { get; set; }
 
-    [MPKey(7), MP2Key(7)]
+    [MPKey(6), MP2Key(6)]
     public bool Recalibration { get; set; }
 
-    [MPKey(8), MP2Key(8)]
+    [MPKey(7), MP2Key(7)]
     public string ActivityRules { get; set; } = string.Empty;
 
-    [MPKey(9), MP2Key(9)]
+    [MPKey(8), MP2Key(8)]
     public string RewardRules { get; set; } = string.Empty;
 
-    [MPKey(10), MP2Key(10)]
+    [MPKey(9), MP2Key(9)]
     public ActivityType ActivityType { get; set; }
 
-    [MPKey(11), MP2Key(11)]
+    [MPKey(10), MP2Key(10)]
     public DateTimeOffset RaffleOpeningTime { get; set; }
 
-    [MPKey(12), MP2Key(12)]
+    [MPKey(11), MP2Key(11)]
     public DateTimeOffset RaffleRevealTime { get; set; }
 
     /// <summary>
     /// 奖品列表
     /// </summary>
-    [MPKey(13), MP2Key(13)]
+    [MPKey(12), MP2Key(12)]
     public RaffleActivityPrizeDTO[]? Prizes { get; set; }
 
     /// <summary>
     /// 条件列表
     /// </summary>
-    [MPKey(14), MP2Key(14)]
+    [MPKey(13), MP2Key(13)]
     public RaffleConditionDTO[]? Conditions { get; set; }
 
-    [MPKey(15), MP2Key(15)]
+    [MPKey(14), MP2Key(14)]
     public string? DealerGameId { get; set; }
 
-    [MPKey(16), MP2Key(16)]
+    [MPKey(15), MP2Key(15)]
     public Guid? GameId { get; set; }
 
-    [MPKey(17), MP2Key(17)]
+    [MPKey(16), MP2Key(16)]
     public string? GameName { get; set; }
 
-    [MPKey(18), MP2Key(18)]
+    [MPKey(17), MP2Key(17)]
     public Guid? GamePlatformId { get; set; }
 
-    [MPKey(19), MP2Key(19)]
+    [MPKey(18), MP2Key(18)]
     public string? GamePlatformName { get; set; }
 
 }
