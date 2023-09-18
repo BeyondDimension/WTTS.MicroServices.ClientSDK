f270bd8216e2d205662c608bfba25853145353ac
BuknSS <284912933@qq.com>
2023-09-18 16:58:39 +0800

    ✨ 添加支付相关的订单信息类

--- src/BD.WTTS.Primitives.Models/Models/Basics/Ordering/OrderInfoDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Basics/Ordering/OrderInfoDTO.cs
@@ -72,6 +72,46 @@ public class OrderInfoDTO : IKeyModel<Guid>
     public DateTimeOffset CreationTime { get; set; }
 }
 
+public class OrderPayInfoDTO : IKeyModel<Guid>
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// 订单号
+    /// </summary>
+    public long OrderNumber { get; set; }
+
+    /// <summary>
+    /// 订单超时时间
+    /// </summary>
+    public DateTimeOffset Timeout { get; set; }
+
+    /// <summary>
+    /// 应收金额
+    /// </summary>
+    public decimal AmountReceivable { get; set; }
+
+    /// <summary>
+    /// 业务类型，关联的支付业务类型枚举
+    /// </summary>
+    public OrderBusinessType BusinessType { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    public string? Remarks { get; set; }
+
+    /// <summary>
+    /// 订单状态
+    /// </summary>
+    public OrderStatus Status { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    public DateTimeOffset CreationTime { get; set; }
+}
+
 public class OrderPaymentCompositionInfoDTO
 {
     /// <summary>
