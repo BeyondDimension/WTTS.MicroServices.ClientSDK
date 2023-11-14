fbac59f9621a8991d042ab59127a4841d318a844
BuknSS <284912933@qq.com>
2023-11-14 11:36:41 +0800

    🐛 fix 在线支付回调完成订单的支付BUG

--- src/BD.WTTS.Primitives.Models/Models/Payment/OrderPaymentSuccessInfo.cs
+++ src/BD.WTTS.Primitives.Models/Models/Payment/OrderPaymentSuccessInfo.cs
@@ -3,9 +3,10 @@ namespace BD.WTTS.Models;
 /// <summary>
 /// 订单支付成功信息
 /// </summary>
+/// <param name="PaymentId">支付Id（支付组成Id）</param>
 /// <param name="OrderNumber">订单号</param>
 /// <param name="PaymentPlatform">支付平台</param>
 /// <param name="PaymentPlatformOrderNumber">支付平台的订单号/交易号</param>
 /// <param name="AmountReceived">实收金额</param>
 /// <param name="PaymentTime">支付时间</param>
-public record OrderPaymentSuccessInfo(string OrderNumber, PaymentType PaymentPlatform, string PaymentPlatformOrderNumber, decimal AmountReceived, string PaymentTime);
+public record OrderPaymentSuccessInfo(Guid PaymentId, string OrderNumber, PaymentType PaymentPlatform, string PaymentPlatformOrderNumber, decimal AmountReceived, string PaymentTime);
