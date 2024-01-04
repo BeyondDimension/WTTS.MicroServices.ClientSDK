09e91ee9c7134568fa01386ae15bf12923737112
BuknSS <284912933@qq.com>
2024-01-04 13:35:02 +0800

    🎨 add MerchantDeduction

---
+++ src/BD.WTTS.Primitives.Models/Models/Payment/MerchantDeductionInfo.cs
@@ -0,0 +1,3 @@
+namespace BD.WTTS.Models;
+
+public record MerchantDeductionInfo(string TotalAmount, string TradeNo, string GmtPayment);
--- src/BD.WTTS.Primitives.Models/Models/Payment/MerchantDeductionSuccessInfo.cs
+++ src/BD.WTTS.Primitives.Models/Models/Payment/MerchantDeductionSuccessInfo.cs
@@ -9,4 +9,6 @@ namespace BD.WTTS.Models;
 /// <param name="PaymentPlatformOrderNumber">支付平台的订单号/交易号</param>
 /// <param name="AmountReceived">实收金额</param>
 /// <param name="PaymentTime">支付时间</param>
-public record OrderPaymentSuccessInfo(Guid PaymentId, string OrderNumber, PaymentType PaymentPlatform, string PaymentPlatformOrderNumber, decimal AmountReceived, DateTimeOffset PaymentTime);
+/// <param name="AgreementNo"></param>
+public record MerchantDeductionSuccessInfo(Guid PaymentId, string OrderNumber, PaymentType PaymentPlatform, string PaymentPlatformOrderNumber, decimal AmountReceived, DateTimeOffset PaymentTime, string AgreementNo)
+    : OrderPaymentSuccessInfo(PaymentId, OrderNumber, PaymentPlatform, PaymentPlatformOrderNumber, AmountReceived, PaymentTime);
