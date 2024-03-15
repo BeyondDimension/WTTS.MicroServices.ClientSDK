7b1232896e221c5b7a1ff28ae9c26e24f1270e95
BuknSS <284912933@qq.com>
2024-03-15 16:15:21 +0800

    🎨 Add Description attribute

--- src/BD.WTTS.Primitives/Enums/Order/RefundChannelType.cs
+++ src/BD.WTTS.Primitives/Enums/Order/RefundChannelType.cs
@@ -9,10 +9,12 @@ public enum RefundChannelType
     /// <summary>
     /// 原路支付平台退回
     /// </summary>
+    [Description("原路支付平台退回")]
     OriginalWay = 1,
 
     /// <summary>
     /// 人工处理
     /// </summary>
+    [Description("人工处理")]
     ManualHandling = 2,
 }
--- src/BD.WTTS.Primitives/Enums/Order/RefundStatus.cs
+++ src/BD.WTTS.Primitives/Enums/Order/RefundStatus.cs
@@ -9,25 +9,30 @@ public enum RefundStatus
     /// <summary>
     /// 未退款
     /// </summary>
+    [Description("未退款")]
     NoRefund = 1,
 
     /// <summary>
     /// 已退款
     /// </summary>
+    [Description("已退款")]
     Refund = 2,
 
     /// <summary>
     /// 同意退款但原路退还失败
     /// </summary>
+    [Description("同意退款但原路退还失败")]
     Fail = 3,
 
     /// <summary>
     /// 被拒绝
     /// </summary>
+    [Description("被拒绝")]
     Refused = 4,
 
     /// <summary>
     /// 退款中
     /// </summary>
+    [Description("退款中")]
     Refunding = 5,
 }
