2e8575b5ec241fc361bdbab1a21facf64abf1428
BuknSS <284912933@qq.com>
2024-03-27 19:20:46 +0800

    🎨 adjust RaffleActivityDTO

--- src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Advertisement/Raffle/RaffleActivityDTO.cs
@@ -4,34 +4,69 @@ namespace BD.WTTS.Models;
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
 public partial class RaffleActivityDTO
 {
+    /// <summary>
+    /// 主键
+    /// </summary>
     [MPKey(0), MP2Key(0)]
     public Guid Id { get; set; }
 
+    /// <summary>
+    /// 游戏 Id
+    /// </summary>
     [MPKey(1), MP2Key(1)]
-    public string ActivityName { get; set; } = string.Empty;
+    public Guid? GameId { get; set; }
 
+    /// <summary>
+    /// 游戏 Id
+    /// </summary>
     [MPKey(2), MP2Key(2)]
-    public string? Describe { get; set; }
+    public string? GameName { get; set; }
 
+    /// <summary>
+    /// 活动名称
+    /// </summary>
     [MPKey(3), MP2Key(3)]
+    public string ActivityName { get; set; } = "";
+
+    /// <summary>
+    /// 封面 Url
+    /// </summary>
+    [MPKey(4), MP2Key(4)]
     public string? CoverUrl { get; set; }
 
-    [MPKey(4), MP2Key(4)]
+    /// <summary>
+    /// 开始时间
+    /// </summary>
+    [MPKey(5), MP2Key(5)]
     public DateTimeOffset StartTime { get; set; }
 
-    [MPKey(5), MP2Key(5)]
+    /// <summary>
+    /// 结束时间
+    /// </summary>
+    [MPKey(6), MP2Key(6)]
     public DateTimeOffset EndTime { get; set; }
 
-    [MPKey(6), MP2Key(6)]
+    /// <summary>
+    /// 活动类型
+    /// </summary>
+    [MPKey(7), MP2Key(7)]
     public ActivityType ActivityType { get; set; }
 
-    [MPKey(7), MP2Key(7)]
+    /// <summary>
+    /// 开奖时间
+    /// </summary>
+    [MPKey(8), MP2Key(8)]
     public DateTimeOffset RaffleOpeningTime { get; set; }
 
-    [MPKey(8), MP2Key(8)]
+    /// <summary>
+    /// 中奖公示时间
+    /// </summary>
+    [MPKey(9), MP2Key(9)]
     public DateTimeOffset? RaffleRevealTime { get; set; }
 
-    [MPKey(9), MP2Key(9)]
+    /// <summary>
+    /// 中奖公示结束时间
+    /// </summary>
+    [MPKey(10), MP2Key(10)]
     public DateTimeOffset? RaffleRevealEndTime { get; set; }
-
 }
