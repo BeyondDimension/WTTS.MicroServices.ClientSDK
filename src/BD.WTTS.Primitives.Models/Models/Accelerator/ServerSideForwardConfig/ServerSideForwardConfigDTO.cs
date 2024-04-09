d9fd84ee742bd72997aef3db574bbc1ecc92963e
wangyingpan <tel15111138092@gmail.com>
2024-04-09 15:43:30 +0800

    🎨 撤销使用 net8集合初始化方式改为原来的 Array.Empty

--- src/BD.WTTS.Primitives.Models/Models/Accelerator/ServerSideForwardConfig/ServerSideForwardConfigDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Accelerator/ServerSideForwardConfig/ServerSideForwardConfigDTO.cs
@@ -29,7 +29,7 @@ public sealed partial record class ServerSideForwardConfigDTO
         Remarks = remarks;
     }
 
-    public ServerSideForwardConfigDTO() : this(string.Empty, string.Empty, [], null, null)
+    public ServerSideForwardConfigDTO() : this(string.Empty, string.Empty, Array.Empty<ServerSideForwardDestDTO>(), null, null)
     {
     }
 }
