1c06f22ea9f51db3bfdaed19b3392d5fbda00020
wangyingpan <tel15111138092@gmail.com>
2024-04-10 11:38:05 +0800

    添加空构造函数用于反序列化

--- src/BD.WTTS.Primitives.Models/Models/Accelerator/ServerSideForwardConfig/ServerSideForwardDestMappingDTO.cs
+++ src/BD.WTTS.Primitives.Models/Models/Accelerator/ServerSideForwardConfig/ServerSideForwardDestMappingDTO.cs
@@ -3,10 +3,15 @@ namespace BD.WTTS.Models;
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
 public sealed partial record class ServerSideForwardDestMappingDTO
 {
+    [MPConstructor, MP2Constructor]
     public ServerSideForwardDestMappingDTO(ServerSideForwardDestDTO from, ServerSideForwardDestDTO to)
     {
         From = from;
         To = to;
+    }
+
+    public ServerSideForwardDestMappingDTO() : this(new(string.Empty, string.Empty), new(string.Empty, string.Empty))
+    {
     }
 
     [MPKey(0), MP2Key(0)]
