0c179e47d33133a01b2b12280700a866a4e1a721
BuknSS <284912933@qq.com>
2024-11-19 19:12:43 +0800

    🎨 AppVersionDTO_v1 AppVersionDTODownload_v1 用作新模型，旧的保持名称不变

--- src/BD.WTTS.Primitives.Models/Models/Basics/AppVersionDTO.v1.cs
+++ src/BD.WTTS.Primitives.Models/Models/Basics/AppVersionDTO.v1.cs
@@ -2,7 +2,7 @@
 namespace BD.WTTS.Models;
 
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
-public sealed partial class AppVersionDTO : IKeyModel<Guid>, IExplicitHasValue
+public sealed partial class AppVersionDTO_v1 : IKeyModel<Guid>, IExplicitHasValue
 {
     [MPKey(0), MP2Key(0)]
     public Guid Id { get; set; }
@@ -41,7 +41,7 @@ public sealed partial class AppVersionDTO : IKeyModel<Guid>, IExplicitHasValue
     /// 下载类型与下载地址
     /// </summary>
     [MPKey(6), MP2Key(6)]
-    public List<AppVersionDTODownload>? Downloads { get; set; }
+    public List<AppVersionDTODownload_v1>? Downloads { get; set; }
 
     bool IExplicitHasValue.ExplicitHasValue()
     {
--- src/BD.WTTS.Primitives.Models/Models/Basics/AppVersionDTODownload.v1.cs
+++ src/BD.WTTS.Primitives.Models/Models/Basics/AppVersionDTODownload.v1.cs
@@ -1,12 +1,28 @@
 // ReSharper disable once CheckNamespace
 namespace BD.WTTS.Models;
 
-/// <summary>
-/// V1版本
-/// </summary>
 [MPObj, MP2Obj(SerializeLayout.Explicit)]
-public sealed partial class AppVersionDTODownloadV1 : AppVersionDTODownload
+public partial class AppVersionDTODownload_v1 : IExplicitHasValue
 {
+    /// <inheritdoc cref="CloudFileType"/>
+    [MPKey(0), MP2Key(0)]
+    public CloudFileType DownloadType { get; set; }
+
+    [MPKey(1), MP2Key(1)]
+    public string? SHA256 { get; set; }
+
+    [MPKey(2), MP2Key(2)]
+    public long Length { get; set; }
+
+    [MPKey(3), MP2Key(3)]
+    public string DownloadUrl { get; set; } = "";
+
+    [MPKey(4), MP2Key(4)]
+    public UpdateChannelType DownloadChannelType { get; set; }
+
+    [MPKey(5), MP2Key(5)]
+    public string? SHA384 { get; set; }
+
     /// <summary>
     /// 是否增量更新
     /// </summary>
@@ -25,4 +41,13 @@ public sealed partial class AppVersionDTODownloadV1 : AppVersionDTODownload
 #endif
     public string[] DownloadUrlArray
         => ApiConstants.GetSplitValues(DownloadUrl);
+
+    bool IExplicitHasValue.ExplicitHasValue()
+    {
+        return ((SHA256 != null && SHA256.Length == Hashs.String.Lengths.SHA256) || (SHA384 != null && SHA384.Length == Hashs.String.Lengths.SHA384)) &&
+            String2.IsHttpUrl(DownloadUrl, true) &&
+            Length > 0 &&
+            DownloadType.IsDefined() &&
+            DownloadChannelType != UpdateChannelType.Auto && DownloadChannelType.IsDefined();
+    }
 }
